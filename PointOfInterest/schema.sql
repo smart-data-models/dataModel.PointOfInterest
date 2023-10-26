0b614f01f9ceaaac58501383bf63484309f09284
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:55:15 +0200

    test of SQL export

--- PointOfInterest/schema.sql
+++ PointOfInterest/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model PointOfInterest of the subject dataModel.PointOfInterest for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE PointOfInterest_type AS ENUM ('PointOfInterest');
-CREATE TABLE PointOfInterest (address JSON, alternateName TEXT, areaServed TEXT, category JSON, contactPoint JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, municipalityInfo JSON, name TEXT, owner JSON, refSeeAlso JSON, source TEXT, type PointOfInterest_type, wardId TEXT, zoneId TEXT, zoneName TEXT);
+CREATE TABLE PointOfInterest (address JSON, alternateName TEXT, areaServed TEXT, category JSON, contactPoint JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, municipalityInfo JSON, name TEXT, owner JSON, refSeeAlso JSON, seeAlso JSON, source TEXT, type PointOfInterest_type, wardId TEXT, zoneId TEXT, zoneName TEXT);
